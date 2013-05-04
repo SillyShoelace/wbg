3a2e667d939fbd1a99aa7088c1df01b94f64e47c
mike-t <mickwalton@gmail.com>
2013-05-04 19:50:31 +1000

    test project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wbg::Application.config.secret_token = '4980f40791f8f2332c3207ac5d03f632f8e85714f58d6887cef3a9a2d50fb3e2ad02560ab945bb5c28f34553761a091af1258977f1b56641e2609286c48e043b'
